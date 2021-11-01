539c2ca53c8abc96e89d8132c8c3f342a59af17d
Ana Lucia Alvarez <analucia.alvarez@auben.net>
2021-11-01 18:26:36 -0300

    adding temp_min assertions

--- cypress/integration/open-weather/weather/weather.js
+++ cypress/integration/open-weather/weather/weather.js
@@ -9,6 +9,13 @@ Given('A city name London', function (city) {
     //this.context.request = city;
 });
 
+
+Given('A city name Londonnnn', function (city) {
+    cy.log(city);
+    //TODO
+    //this.context.request = city;
+});
+
 Given('The latitude {string} and the longitud {string}', function (lat, lon) {
     //TODO
     //this.context.request = city;
@@ -22,10 +29,12 @@ Given('The ZIP code {string}', function (zip) {
 When('I send GET request to weather service api endpoint', async function (path) {
     cy.request({
         method: 'GET',
-        url: Cypress.env('baseUrl') + '/weather?q=' + CITY.Name + '&appid=' + Cypress.env('apiKey')
+        url: Cypress.env('baseUrl') + '/weather?q=' + CITY.Name + '&appid=' + Cypress.env('apiKey'),
+        failOnStatusCode: false
     })
     .should(response => {
         cy.task('pushValue', { name: 'responseCode', value: response.status })
+        cy.task('pushValue', { name: 'temp_min', value: response.body.main.temp_min })
         cy.log(JSON.stringify(response.body));
     });
 });
@@ -33,6 +42,16 @@ When('I send GET request to weather service api endpoint', async function (path)
 Then('I get response code {int}', async function (code) {
     cy.task('getValue', 'responseCode').then((responseCode) => {
         assert.equal(responseCode, code);
+    });
+});
+
+Then('I get response code is {int} and temp_min between {int} and {int}', async function (code, min, max) {
+    cy.task('getValue', 'responseCode').then((responseCode) => {
+        assert.equal(responseCode, code);
+    });
+    cy.task('getValue', '   temp_min').then((temp_min) => {
+        assert.isAtLeast(temp_min, min);
+        assert.isAtMost(temp_min, max);
     });
 });
 
